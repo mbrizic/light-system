b4ceaeacb84cce425e2ac23b891129140428a3fb
Mario <brizic.mario@gmail.com>
2016-09-06 19:22:38 +0200

    More unimportant UI and architecture changes

--- frontend/models/scene.js
+++ frontend/models/scene.js
@@ -4,9 +4,12 @@ function SceneFactory() {
 	function SceneConstructor(sceneViewModel) {
 		var scene = sceneViewModel;
 
+		scene.hasLights = function () {
+			return scene.lights && scene.lights.length > 0;
+		}
+
 		scene.isTurnedOn = function () {
-			var hasLights = scene.lights && scene.lights.length > 0;
-			return hasLights && scene.lights.every(light => light.isTurnedOn());
+			return scene.hasLights() && scene.lights.every(light => light.isTurnedOn());
 		};
 
 		return scene;
